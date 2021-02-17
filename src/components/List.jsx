6f0f400856b6a742ac8a5047447a05c2608cde30
corentin.leb <leberre.corentin@gmail.com>
2021-02-17 15:02:00 +0100

    adding css style

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,13 +2,13 @@ const List = ({student}) => {
     return (
     <>
     {student.map((eleve)=>{
-        const { id , name , attendance} = eleve;
+        const {id , name , attendance} = eleve;
     
         return (
             <article className="student" key={id}>
-                <div>
+                <div className="student-list">
                     <h4>{name}</h4>
-                    <p>{attendance ? 'Présent' : 'Absent'}</p>
+                    <p>{student.attendance ? 'Présent' : 'Absent'}</p>  
                 </div>
             </article>
         )
