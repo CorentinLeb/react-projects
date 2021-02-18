b9d8a77c83bceef0dd6fb0a9b72d471e1915752c
corentin.leb <leberre.corentin@gmail.com>
2021-02-18 15:53:17 +0100

    adding routeur

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import React, { useState } from 'react';
 import './App.css';
 import data from './data';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   const [student, setStudent] = useState(data);
   return (
-    <main>
-      <section className="container">
-      <Navbar />
-        <h2>Attendance List for {student.length} students</h2>
-        <List student={student}/>
-        <button onClick = {()=>setStudent([])}>Clear list</button>
-      </section>
-    </main>
+    <Router>
+      <main>
+        <Navbar/>
+        <Switch>
+          <Route path="/">
+            <section className="container">
+              <h2>Attendance List for {student.length} students</h2>
+              <List student={student}/>
+              <button onClick = {()=>setStudent([])}>Clear list</button>
+            </section>
+          </Route>
+        </Switch> 
+      </main>
+    </Router>
   );
 }
 
