f7473ef184d53941edc18340517f309721d10ff4
corentin.leb <leberre.corentin@gmail.com>
2021-02-18 13:45:12 +0100

    adding navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import List from './components/List';
 import React, { useState } from 'react';
 import './App.css';
 import data from './data';
+import Navbar from './components/Navbar';
 
 function App() {
   const [student, setStudent] = useState(data);
   return (
     <main>
       <section className="container">
+      <Navbar />
         <h2>Attendance List for {student.length} students</h2>
         <List student={student}/>
         <button onClick = {()=>setStudent([])}>Clear list</button>
